f23cde6b5b2af486fb6ef1d79367618962673acc
Nachiket Jadhav <nachiketjadhav123@yahoo.co.in>
2021-08-29 12:13:43 +0530

    Added Files

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi vector<int>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rep(i, n) for (int i = 0; i < n; i++)
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+
+void solve()
+{
+    int i, j, k, n, m;
+    vector<int> a;
+}
+int main()
+{
+    fast 
+    
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+} 
